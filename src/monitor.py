269eb046c296206b296b9cf74b14c884d4989a20
Syed Farooq Ahamed <157580232+syed-farooqahamed@users.noreply.github.com>
2025-09-04 01:44:07 +0530

    Initial commit: Cloud Resource Monitoring project

--- src/monitor.py
+++ src/monitor.py
@@ -1,12 +1,16 @@
 import psutil
 import time
+import google.cloud.logging
 import logging
+from google.cloud.logging.handlers import CloudLoggingHandler
+from google.cloud.logging_v2.resource import Resource
 
-logging.basicConfig(
-    filename="system_monitor.log",
-    level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(message)s"
-)
+client = google.cloud.logging.Client()
+resource = Resource(type="global", labels={})
+cloud_handler = CloudLoggingHandler(client, resource=resource)
+cloud_logger = logging.getLogger("system-monitor")
+cloud_logger.setLevel(logging.INFO)
+cloud_logger.addHandler(cloud_handler)
 
 def get_system_usage():
     cpu = psutil.cpu_percent(interval=1)
@@ -16,11 +20,12 @@ def get_system_usage():
 
 def log_usage():
     cpu, memory, disk = get_system_usage()
-    logging.info(f"CPU: {cpu}%, Memory: {memory}%, Disk: {disk}%")
-    print(f"CPU: {cpu}%, Memory: {memory}%, Disk: {disk}%")
+    msg = f"CPU: {cpu}%, Memory: {memory}%, Disk: {disk}%"
+    cloud_logger.info(msg)
+    print(msg)
 
 if __name__ == "__main__":
-    print("Starting system monitoring... (Press Ctrl+C to stop)")
+    print("Starting Cloud Monitoring... (Press Ctrl+C to stop)")
     try:
         while True:
             log_usage()
